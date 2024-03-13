872deec9634d4676a54c1e891cd1b2c8afb42530
MattiaBologna <mattiabologna01@gmail.com>
2024-03-13 17:15:01 +0100

    Add: change difficulties

--- js/app.js
+++ js/app.js
@@ -9,7 +9,26 @@ playButtonDOMElement.addEventListener('click', function() {
     const gridContainerDOMElement = document.querySelector('.grid__container')
     // console.log(gridContainerDOMElement);
 
-    const size = 10;
+    // RIMUOVERE EVENTUALI GRIGLIE PRECEDENTI PER EVITARE DI GENERARE DUE GRIGLIE UNA SOTTO L'ALTRA
+    gridContainerDOMElement.innerHTML = ''
+    playButtonDOMElement.innerHTML = 'Restart'
+
+    let size = 10;
+
+    // prendi il select 
+    const changeDifficultyDOMElement = document.getElementById('difficulty__grade')
+    // prendi il valore del select 
+    const changeDifficultyValue = changeDifficultyDOMElement.value 
+    
+    // seleziona il numero di celle da generare in base alla difficoltà scelta
+    if (changeDifficultyValue === '1') {
+        size = 10
+    } else if (changeDifficultyValue === '2') {
+        size = 9
+    } else {
+        size = 7
+    }
+
     const numberOfCells = size ** 2;
 
     // generare le celle 
@@ -21,12 +40,23 @@ playButtonDOMElement.addEventListener('click', function() {
         cellsDOMELement.className = 'cell'
         cellsDOMELement.innerHTML = cellNumber
 
+        // aggiungere la classe relativa alla difficoltà corrente per scegliere quante colonne avere
+        if (size === 10) {
+            gridContainerDOMElement.classList.add('grid__hard')
+        } else if (size === 9) {
+            gridContainerDOMElement.classList.add('grid__normal')
+        } else if (size === 7) {
+            gridContainerDOMElement.classList.add('grid__easy')
+        }
+
         // aggiungere le celle all'html
         gridContainerDOMElement.append(cellsDOMELement)
         // console.log(cellsDOMELement);
 
+        // aggiungere/rimuovere la classe bg__blue al click sulla cella 
         cellsDOMELement.addEventListener('click', function(){
             this.classList.toggle('bg__blue')
+            console.log(cellNumber);
         })
     }
 })
